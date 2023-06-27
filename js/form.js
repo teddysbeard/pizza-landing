a7439fa1a1131c49e8a881621685a0abb45cff56
teddysbeard <d.komarov@ivel.design>
2023-06-27 18:05:24 +0300

    feat:add mail.php

--- js/form.js
+++ js/form.js
@@ -4,9 +4,38 @@
     if (forms.length === 0) {
         return;
     }
-    /* Функция отправляет форму */
+    //  'name=value&name2=value2'
+    
+    let serialize = function(form) {
+        let items = form.querySelectorAll('input, select, textarea');
+        let str = '';
+        for (let i = 0; i < items.length; i += 1) {
+            let item = items[i];
+            let name = item.name;
+            let value = item.value;
+            if (name) {
+                str += name + '=' + value;
+            }
+        }
+    }
+
+    /* Функция отправляет форму через Ajax*/
     let formSend = function(form) {
-        console.log(form);
+        // let data = 'name=value&name2=value2'
+        let data = serialize(form);
+        console.log(data);
+        return;
+        let xhr = new XMLHttpRequest();
+        let url = 'mail/mail.php';
+
+        xhr.open('POST', url);
+        // xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        xhr.onload = function() {
+            // console.log('done');
+            console.log(xhr.response);
+        };
+        xhr.send(data);
     }
 
     /* -------------------------- */
---
+++ mail/mail.php
@@ -0,0 +1,46 @@
+<?php
+    // echo 'hello world';
+    $method = $_SERVER['REQUEST_METHOD'];
+    if ($method !== 'POST') {
+        exit();
+    }
+    $project_name = 'PizzaTime';
+    $admin_email = 'dmitriy.komarov.1998@mail.ru';
+    $form_subject = 'Заявка на доставку';
+    // $message = 'HELLO WORLD';
+    $message = '';
+    $color_counter = 1;
+
+    foreach($_POST as $key => $value) {
+        if ($value === '') {
+            continue;
+        }
+        $color = $color_counter % 2 === 0 ? '#fff' : '#f8f8f8';
+        $message .=  "
+            <tr style='background-color: $color;'>
+                <td style='padding: 10px; border: 1px solid #e9e9e9;'>$key</td>
+                <td style='padding: 10px; border: 1px solid #e9e9e9;'>$value</td>
+            </tr>";
+        $color_counter ++;
+    }
+
+    $message = "<table style='width: 100%;'>$message</table>"
+   
+    // $message = '<table style="width: 100%;">
+    // <tr style="background-color: #f8f8f8;">
+    // <td style="padding: 10px; border: 1px solid #e9e9e9;">Name</td>
+    // <td style="padding: 10px; border: 1px solid #e9e9e9;">Value</td>
+    // </tr>
+    // <tr style="background-color: #f8f8f8;">
+    //     <td style="padding: 10px; border: 1px solid #e9e9e9;">Name</td>
+    //     <td style="padding: 10px; border: 1px solid #e9e9e9;">Value</td>
+    //     </tr>
+    // </table>'
+
+    $headers  = "MIME-Version: 1.0\r\n"; 
+    $headers .= "Content-type: text/plain; charset=utf-8\r\n";
+    $headers .= "From: $admin_email\r\n";
+
+    
+    mail($project_name,$form_subject,$message, $headers);
+    ?>
