bbfc78be2c5148b82977e59f5bf266971fed7708
teddysbeard <d.komarov@ivel.design>
2023-07-03 10:14:13 +0300

    feat: add sending mail

--- mail/mail.php
+++ mail/mail.php
@@ -1,33 +1,33 @@
 <?php
-    // echo 'hello world';
-    $method = $_SERVER['REQUEST_METHOD'];
-    if ($method !== 'POST') {
-        exit();
-    }
-    $to = 'dmitriy.komarov.1998@mail.ru';
-    $project_name = 'PizzaTime';
-    $admin_email = 'dmitriy.komarov.1998@mail.ru';
-    $form_subject = 'Заявка на доставку';
-    // $message = 'HELLO WORLD';
-    $message = '';
-    $color_counter = 1;
+    // // echo 'hello world';
+    // $method = $_SERVER['REQUEST_METHOD'];
+    // if ($method !== 'POST') {
+    //     exit();
+    // }
+    // $to = 'dmitriy.komarov.1998@mail.ru';
+    // $project_name = 'PizzaTime';
+    // $admin_email = 'dmitriy.komarov.1998@mail.ru';
+    // $form_subject = 'Заявка на доставку';
+    // // $message = 'HELLO WORLD';
+    // $message = '';
+    // $color_counter = 1;
 
-    print_r($_POST);
-    exit();
-    foreach($_POST as $key => $value) {
-        if ($value === '') {
-            continue;
-        }
-        $color = $color_counter % 2 === 0 ? '#fff' : '#f8f8f8';
-        $message .=  "
-            <tr style='background-color: $color;'>
-                <td style='padding: 10px; border: 1px solid #e9e9e9;'>$key</td>
-                <td style='padding: 10px; border: 1px solid #e9e9e9;'>$value</td>
-            </tr>";
-        $color_counter ++;
-    }
+    // print_r($_POST);
+    // exit();
+    // foreach($_POST as $key => $value) {
+    //     if ($value === '') {
+    //         continue;
+    //     }
+    //     $color = $color_counter % 2 === 0 ? '#fff' : '#f8f8f8';
+    //     $message .=  "
+    //         <tr style='background-color: $color;'>
+    //             <td style='padding: 10px; border: 1px solid #e9e9e9;'>$key</td>
+    //             <td style='padding: 10px; border: 1px solid #e9e9e9;'>$value</td>
+    //         </tr>";
+    //     $color_counter ++;
+    // }
 
-    $message = "<table style='width: 100%;'>$message</table>";
+    // $message = "<table style='width: 100%;'>$message</table>";
    
     // $message = '<table style="width: 100%;">
     // <tr style="background-color: #f8f8f8;">
@@ -40,16 +40,43 @@
     //     </tr>
     // </table>'
 
-    $headers  = "MIME-Version: 1.0\r\n"; 
-    // $headers .= "Content-type: text/plain; charset=utf-8\r\n";
-    $headers .= "Content-type: text/html; charset=utf-8\r\n";
-    $headers .= "From: $admin_email\r\n";
+    // $headers  = "MIME-Version: 1.0\r\n"; 
+    // // $headers .= "Content-type: text/plain; charset=utf-8\r\n";
+    // $headers .= "Content-type: text/html; charset=utf-8\r\n";
+    // $headers .= "From: $admin_email\r\n";
 
     
-    $success_send = mail($to, $project_name, $form_subject, $message, $headers);
-    if ($success_send) {
-        echo "success";
-    } else {
-        echo 'error';
-    }
+    // $success_send = mail($to, $project_name, $form_subject, $message, $headers);
+    // if ($success_send) {
+    //     echo "success";
+    // } else {
+    //     echo 'error';
+    // }
+    $headers  = 'MIME-Version: 1.0' . "\r\n";
+    $headers .= 'dmitriy.komarov.1998@mail.ru';
+    $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+    $body = '';
+    foreach($_POST as $key => $value) {
+        if ($value === '') {
+            continue;
+        }
+        $color = $color_counter % 2 === 0 ? '#fff' : '#f8f8f8';
+        $body .=  "
+            <tr style='background-color: $color;'>
+                <td style='padding: 10px; border: 1px solid #e9e9e9;'>$key</td>
+                <td style='padding: 10px; border: 1px solid #e9e9e9;'>$value</td>
+            </tr>";
+        $color_counter ++;
+    };
+   $body = "<table style='width: 100%;'>$body</table>";
+   $to_email = "dmitriy.komarov.1998@mail.ru";
+   $subject = "Simple Email Test via PHP";
+//    $body = "Hi,\n This is test email send by PHP Script";
+    // $headers .= "From: $admin_email\r\n";
+ 
+   if ( mail($to_email, $subject, $body, $headers)) {
+      echo("Email successfully sent to $to_email...");
+   } else {
+      echo("Email sending failed...");
+   }
     ?>
